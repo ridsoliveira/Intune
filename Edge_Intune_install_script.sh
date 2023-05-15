1f6ba38acf7663af29610b45886172378617f230
Ricardo da Silva Oliveira <ricardodso@outlook.com>
2023-05-15 12:56:52 -0300

    Update Edge_Intune_install_script.sh
    Atualizando o Edge para versão "stable"

--- Edge_Intune_install_script.sh
+++ Edge_Intune_install_script.sh
@@ -3,15 +3,23 @@
 # Verificar se o Microsoft Edge já está instalado
 if ! command -v msedge &> /dev/null; then
   # Instalação do Microsoft Edge
+  echo "Instalação do Microsoft Edge. Aguarde..."
+  sleep 3
 
   # Verificar a versão do Ubuntu
   ubuntu_version=$(lsb_release -r -s)
 
   # Baixar o pacote DEB do Microsoft Edge
-  if [[ $ubuntu_version == "20.04" ]]; then
-    wget "https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-dev/microsoft-edge-dev_93.0.946.0-1_amd64.deb" -O edge.deb
-  elif [[ $ubuntu_version == "22.04" ]]; then
-    wget "https://packages.microsoft.com/repos/edge/pool/main/m/microsoft-edge-dev/microsoft-edge-dev_93.0.946.0-1_amd64.deb" -O edge.deb
+  if [[ $ubuntu_version == "20.04" || $ubuntu_version == "22.04"]]; then
+    ## Setup
+    # Instalar chave de assinatura do pacote Microsoft
+    curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+    sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/
+    sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-beta.list'
+    sudo rm microsoft.gpg
+    ## Install
+    sudo apt update
+    sudo apt install microsoft-edge-beta
   else
     echo "Versão do Ubuntu não suportada."
     exit 1
@@ -25,6 +33,7 @@ if ! command -v msedge &> /dev/null; then
   rm edge.deb
 else
   echo "Microsoft Edge já está instalado."
+  sleep 5
 fi
 
 # Verificar a versão do Ubuntu
@@ -32,32 +41,9 @@ ubuntu_version=$(lsb_release -r -s)
 
 if [[ $ubuntu_version == "20.04" ]]; then
   # Instalação para Ubuntu 20.04
-
-  # Instalar Curl
-  sudo apt install curl gpg -y
-
-  # Instalar chave de assinatura do pacote Microsoft
-  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-  sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/
-  sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/ubuntu/20.04/prod focal main" > /etc/apt/sources.list.d/microsoft-ubuntu-focal-prod.list'
-  sudo rm microsoft.gpg
-
   # Instalar o aplicativo Microsoft Intune
-  sudo apt update
-  sudo apt install intune-portal -y
-elif [[ $ubuntu_version == "22.04" ]]; then
-  # Instalação para Ubuntu 22.04
-
-  # Instalar Curl
-  sudo apt install curl gpg -y
-
-  # Instalar chave de assinatura do pacote Microsoft
-  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-  sudo install -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/
-  sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/microsoft.gpg] https://packages.microsoft.com/ubuntu/22.04/prod jammy main" > /etc/apt/sources.list.d/microsoft-ubuntu-jammy-prod.list'
-  sudo rm microsoft.gpg
-
-  # Instalar o aplicativo Microsoft Intune
+  echo "Instalar o aplicativo Microsoft Intune. Aguarde..."
+  sleep 3
   sudo apt update
   sudo apt install intune-portal -y
 else
@@ -66,4 +52,6 @@ else
 fi
 
 # Reinicialize o dispositivo
+echo "Reinicializando o dispositivos em 30 segundos. Aguarde..."
+sleep 3
 sudo reboot
